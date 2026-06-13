eff62dad8330f1ede14d9936b150eaecc285e13c
biancabp <bianca.bezerra.713@ufrn.edu.br>
2026-06-13 15:47:17 -0300

    feat: classes do modelo: genero e artista

---
+++ src/models/artista.py
@@ -0,0 +1,7 @@
+class artista:
+    def __init__(self, id, nome, genero_id, pais=None, descricao=None):
+        self.id = id
+        self.nome = nome
+        self.genero_id = genero_id
+        self.pais = pais
+        self.descricao = descricao
---
+++ src/models/genero.py
@@ -0,0 +1,5 @@
+class genero:
+    def __init__(self, id, nome, descricao=None):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
