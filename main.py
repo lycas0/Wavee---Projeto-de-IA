85d0bb639478e349ebb8fd931413585171d37501
Marcus <omarcusvinicius96@gmail.com>
2026-06-13 17:30:02 -0300

    adicionando o aprendizado por feedback no main.py

--- main.py
+++ main.py
@@ -17,17 +17,46 @@ def main():
             continue
 
         resultado = agente.agir(entrada)
+    
+        musicas_recomendadas = resultado["musicas"] #guardar o resultado para usar como lista de opções para o usuário
 
         print(f"\nGênero identificado: {resultado['genero']}")
         print("Probabilidades por gênero:")
         for genero, prob in sorted(resultado["probabilidades"].items(), key=lambda x: -x[1]):
             print(f"  {genero}: {prob:.2%}")
 
+        #print("\nRecomendações:")
+        #for m in resultado["musicas"]:
+            #print(f"  - {m.nome} ({m.cantor})")
+
         print("\nRecomendações:")
-        for m in resultado["musicas"]:
-            print(f"  - {m.nome} ({m.cantor})")
+        for i, m in enumerate (musicas_recomendadas, start=1):
+            print(f"  [{i}] - {m.nome} ({m.cantor})")
 
         print()
+
+        #usando a função registrar_feedback na main
+        if musicas_recomendadas:
+            try:
+                escolha = int(input("Digite o número da música que você deseja ouvir, ou digite 0: ").strip())
+
+                if 1 <= escolha <= len(musicas_recomendadas):
+                    musica_escolhida = musicas_recomendadas[escolha - 1] 
+
+                    print(f"Tocando: {musica_escolhida.nome}")
+
+                    agente.registrar_feedback(
+                        entrada_texto=entrada,
+                        genero_nome=resultado['genero'],
+                        musica_id=musica_escolhida.id,
+                        gostou=True
+                    ) #aqui o agente guarda a interação e aprende com ela 
+                else:
+                    print("Nenhuma música foi selecionada")
+
+            except ValueError:
+                print("\nOpção inválida, volte para a busca")
+                
 
     agente.encerrar()
     print("Até mais!")
